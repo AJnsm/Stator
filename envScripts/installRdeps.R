a22810f9576a2d448b9a7e33badc2aded0dafdba
Abel Jansma <abel_jansma@hotmail.com>
2020-12-15 09:56:32 +0100

    fixed broken rEnv_eddie yaml.

--- envScripts/installRdeps.R
+++ envScripts/installRdeps.R
@@ -14,3 +14,7 @@ install.packages("pcalg")
 BiocManager::install("Rgraphviz")
 
 install.packages("BiDAG")
+
+
+## Dependencies: conda create -n rEnv r-base=4.0.3  bioconductor-graph bioconductor-rbgl r-ggm r-pcalg bioconductor-rgraphviz r-stringr
+## Then add BiDAG from within R, make sure .libPaths() is pointing to the rEnv directory. 
