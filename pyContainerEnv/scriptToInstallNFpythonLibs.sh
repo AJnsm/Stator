a9a92928d042f5a9ebf373c0d52ab5b92aea7078
Abel Jansma <abel_jansma@hotmail.com>
2020-12-17 18:45:41 +0100

    added docker and singularity configs and Dockerfiles for R and Python

--- pyContainerEnv/scriptToInstallNFpythonLibs.sh
+++ pyContainerEnv/scriptToInstallNFpythonLibs.sh
@@ -1,6 +1,6 @@
-#first create and activate env:
-conda create -n NF_TL_env 
-conda activate NF_TL_env
+#could first create and activate env, but does not seem necessary:
+#conda create -n NF_TL_env 
+#conda activate NF_TL_env
 # See also /Users/s1855283/anaconda3/envs/NF_TL_env/conda-meta/history|grep 'cmd'
 
 conda install python=3.6
---
+++ rContainerEnv/installLibs.R
@@ -0,0 +1,18 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("graph")
+
+BiocManager::install("RBGL")
+
+BiocManager::install("graph")
+
+install.packages("ggm")
+
+install.packages("pcalg")
+
+BiocManager::install("Rgraphviz")
+
+install.packages("BiDAG")
+
+install.packages("stringr")
