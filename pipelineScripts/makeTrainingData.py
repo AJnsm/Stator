9bce473326231d24a2357090660a0cdf32d81f47
Abel Jansma <abel_jansma@hotmail.com>
2021-11-22 16:32:05 +0100

    added new makeData mode: expression

--- pipelineScripts/makeTrainingData.py
+++ pipelineScripts/makeTrainingData.py
@@ -44,7 +44,7 @@ twoReps = bool(args.twoReplicates)
 
 
 
-# ****************** data-agnostic QC analysis and selection: ******************
+# ****************** data-agnostic  ******************
 
 
 if args.dataType=='agnostic':
@@ -150,7 +150,120 @@ if args.dataType=='agnostic':
 
 
 
+# ****************** General expression-data QC analysis and selection: ******************
 
+
+if args.dataType=='expression':
+
+print("loading data")
+    scObj = sc.read_csv(rawData)
+    print("data loaded!")
+
+    scObj.var_names_make_unique()
+    scObj.obs['index'] = np.arange(len(scObj))
+
+
+    try:
+        print('loading user-defined genes')
+        print('file with user genes:  ', args.userGenes)
+        userGenes = pd.read_csv(args.userGenes).columns.values
+        userGenes = [g for g in userGenes if g in scObj.var.index]
+        nGenes = max(nGenes, len(userGenes))
+    except Exception as e:
+        print(e)
+        print('NOTE: continuing without user-defined genes')
+        userGenes = np.array([])
+
+
+    print('adding doublet and cluster data')
+
+
+    try:
+        print('loading cluster data')
+        clusters = pd.read_csv(args.clusters, index_col=0)
+        scObj.obs['cluster'] = clusters.values
+        cl = int(args.cluster)
+
+    except:
+        print('NOTE: continuing without cluster file.')
+        scObj.obs['doublet'] = False
+        scObj.obs['cluster'] = 1
+        cl = 1
+
+    try:
+        print('loading doublet data')
+        doubs = pd.read_csv(args.bcDoublets[0], index_col=0)
+        scObj.obs['doublet'] = doubs
+    except:
+        print('NOTE: continuing without doublet annotation.')
+        scObj.obs['doublet'] = False
+        
+    scObj = scObj[(scObj.obs['doublet']==False) & (scObj.obs['cluster']==cl)]
+
+    # Remove cells with high mito, low n_genes.
+    sc.pp.filter_cells(scObj, min_genes=len(scObj.var.index)*0.02)
+
+    mito_genes = scObj.var_names.str.startswith('mt-')
+    scObj.obs['percent_mito'] = np.sum(
+        scObj[:, mito_genes].X, axis=1).A1 / np.sum(scObj.X, axis=1).A1
+
+    scObj = scObj[scObj.obs['percent_mito']<.12]
+
+    sc.pl.violin(scObj, ['n_genes'],
+                 jitter=0.4, multi_panel=True, save='QC_n_genes_CL'+'{:0>2}'.format(cl) + '_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.png')
+
+    sc.pl.violin(scObj, ['percent_mito'],
+                 jitter=0.4, multi_panel=True, save='QC_percent_mito_CL'+'{:0>2}'.format(cl) + '_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.png')
+
+
+
+
+    sc.pp.normalize_total(scObj, target_sum=1e6)
+    sc.pp.log1p(scObj)
+    scObj.raw = scObj
+    sc.pp.highly_variable_genes(scObj, min_mean=0.0125, max_mean=7, min_disp=0.2)
+    sc.pl.highly_variable_genes(scObj, save=f'QC_HVG_selection_CL'+'{:0>2}'.format(cl) + '_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.png')
+    print('selected genes: ', sum(scObj.var['highly_variable']))
+
+    # ------------ add embedding coords -------------
+    print('Running PCA...')
+    sc.tl.pca(scObj, n_comps=2)
+
+    print('Running UMAP...')
+    sc.pp.neighbors(scObj)
+    sc.tl.umap(scObj)
+    print('DONE with embeddings.')
+
+    hvgObj = scObj[:,scObj.var['highly_variable'].values]
+    sorted_HVG = hvgObj.var.sort_values('dispersions_norm', ascending=False).index
+    selected_genes = np.hstack([userGenes, [s for s in sorted_HVG if not s in userGenes]])
+    selected_genes = selected_genes[:nGenes]
+    print('Number of genes selected:   ', selected_genes.shape)
+    scObjBin = scObj.copy()
+    scObjBin.X = (scObjBin.X>0)*1
+
+    sc.pp.subsample(scObjBin, fraction=1.) #Shufle full cluster so that any selection is randomised. 
+
+
+    selectedCellsAndGenes = scObjBin[:,scObjBin.var.index.isin(selected_genes)]
+    clDF = pd.DataFrame(selectedCellsAndGenes.X.toarray())
+    clDF.columns = selectedCellsAndGenes.var.index
+    print('Final QCd data set size: ', clDF.shape)
+
+    print('Two replicates?  ', twoReps)
+    if twoReps:
+        clDF.iloc[:nCells].to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS1_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.csv', index=False)
+        clDF.iloc[nCells:2*nCells].to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS2_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.csv', index=False)
+
+    else:
+        clDF.iloc[:nCells].to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS1_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes.csv', index=False)
+
+
+    pd.DataFrame(scObjBin.obsm['X_pca'][:nCells]).to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS1_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes_PCAcoords.csv', index=False)
+    pd.DataFrame(scObjBin.obsm['X_pca'][nCells:2*nCells]).to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS2_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes_PCAcoords.csv', index=False)
+
+    pd.DataFrame(scObjBin.obsm['X_umap'][:nCells]).to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS1_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes_UMAPcoords.csv', index=False)
+    pd.DataFrame(scObjBin.obsm['X_umap'][nCells:2*nCells]).to_csv('trainingData_CL'+'{:0>2}'.format(cl)+ '_DS2_' + '{:0>5}'.format(nCells) + 'Cells_'+'{:0>4}'.format(nGenes) + 'Genes_UMAPcoords.csv', index=False)
 
 
 
