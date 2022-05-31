11603f4a26390557287ffa98f72bdf474695c515
Abel Jansma <abel_jansma@hotmail.com>
2022-05-31 13:14:32 +0200

    bug fix

--- pipelineScripts/createHOIsummaries.py
+++ pipelineScripts/createHOIsummaries.py
@@ -334,20 +334,22 @@ for order in [3, 4, 5]:
 			plt.savefig(f'{ID}_summary.png')
 			plt.close(fig) 
 
-dsDevDict = {}
+devDict = {}
 for order in [3, 4, 5]:
     for devs, interactors in deviations[f'n{order}']:
         if max(abs(devs))>5:
             ID = '_'.join(genes[interactors])
             maxDevState = format(np.argmax(devs), f"0{order}b")
             
-            dsDevDict[ID] = (maxDevState, np.max(devs))
-            
-strongDeviators = pd.DataFrame.from_dict(dsDevDict).T.sort_values(by=1, ascending=False)
-strongDeviators.columns = ['max dev. state', 'dev']
-strongDeviators.to_csv(f'topDeviatingHOIstates.csv')
+            devDict[ID] = (maxDevState, np.max(devs))
 
+if len(devDict.keys())>0:
+	strongDeviators = pd.DataFrame.from_dict(devDict).T.sort_values(by=1, ascending=False)
+	strongDeviators.columns = ['max dev. state', 'dev']
+	strongDeviators.to_csv(f'topDeviatingHOIstates.csv')
 
+else: 
+	pd.DataFrame(data=[]).to_csv(f'topDeviatingHOIstates.csv')
 
 
 
