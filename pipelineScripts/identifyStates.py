ede8ae00619700bebea93ef675fc3eafbdac4203
Abel Jansma <abel_jansma@hotmail.com>
2022-06-01 12:04:30 +0200

    bug fix

--- pipelineScripts/identifyStates.py
+++ pipelineScripts/identifyStates.py
@@ -17,14 +17,14 @@ print('Modules imported \n')
 
 parser = argparse.ArgumentParser(description='Args for coupling estimation')
 
-parser.add_argument("--dataPath", type=str, help="Path to training data")
-parser.add_argument("--PCApath", type=str, help="Path to PCA embedding of training data")
+parser.add_argument("--trainDatPath", type=str, help="Path to training trainDat")
+parser.add_argument("--PCApath", type=str, help="Path to PCA embedding of training trainDat")
 parser.add_argument("--devStates", type=str, help="Path to list of most deviating states")
 parser.add_argument("--diffCutoff", type=float, help="Dice distance to slice dendrogram")
 
 args = parser.parse_args()
 diffCutoff = args.diffCutoff
-trainDat = pd.read_csv(args.dataPath)
+trainDat = pd.read_csv(args.trainDatPath)
 pcaCoords= pd.read_csv(args.PCApath)
 
 devStates = pd.read_csv(args.devStates, dtype=str)
@@ -85,7 +85,7 @@ def fromImToArr(img):
     '''
     Takes an ImageFile and converts it to a a numpy array of floats so that imshow() can plot it.  
     '''
-    return np.array(img.getdata()).reshape(img.height, img.width, -1)
+    return np.array(img.gettrainDat()).reshape(img.height, img.width, -1)
 
 # Create image labels 
 sns.set_style('white')
@@ -112,7 +112,7 @@ for img, geneStr in enumerate(R_trunc['ivl']):
         # Plot all cells/observations in grey
         plt.plot(pcaCoords.values[:, 0], pcaCoords.values[:, 1], '.', color=(0.5, 0.5, 0.5, 0.05))
         state = [int(x) for x in devStates[devStates['genes']==geneStr]['state'].values[0]]
-        charCells = (data[genes]==state).all(axis=1)
+        charCells = (trainDat[genes]==state).all(axis=1)
         plt.plot(pcaCoords.values[charCells, 0], pcaCoords.values[charCells, 1], 'o', alpha=alph, label = ", ".join(labels))
         cl.append([''.join(s) for s in list(zip(genes, ['+' if x==1 else '-' for x in state]))])
         index+=1
@@ -128,7 +128,7 @@ for img, geneStr in enumerate(R_trunc['ivl']):
         for i in range(index, index+toAdd):
             genes = R_full['ivl'][i].rsplit('_')
             state = [int(x) for x in devStates[devStates['genes']==R_full['ivl'][i]]['state'].values[0]]
-            charCells = (data[genes]==state).all(axis=1)
+            charCells = (trainDat[genes]==state).all(axis=1)
             plt.plot(pcaCoords.values[charCells, 0], pcaCoords.values[charCells, 1], 'o', alpha=alph, label = ", ".join(labels))
             cl.append([''.join(s) for s in list(zip(genes, ['+' if x==1 else '-' for x in state]))])
         index += toAdd
@@ -150,13 +150,13 @@ def add_imgLab(xCoord, yCoord, ID, ax, ds):
     '''
     img = statePlots[ID]
     
-    img = np.array(img.getdata()).reshape(img.height, img.width, -1)
+    img = np.array(img.gettrainDat()).reshape(img.height, img.width, -1)
     img = img[5:-5, 5:-5, :]
     im = OffsetImage(img, zoom=0.34)
     im.image.axes = ax
 
     ab = AnnotationBbox(im, (xCoord, yCoord),  xybox=(0., 30.), frameon=False,
-                        xycoords='data',  boxcoords="offset points", pad=0)
+                        xycoords='trainDat',  boxcoords="offset points", pad=0)
 
     ax.add_artist(ab)
 
