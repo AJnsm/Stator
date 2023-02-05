416f263352d47a2bc403f604ca49f62d0baf71c1
Abel Jansma <abel_jansma@hotmail.com>
2023-02-05 16:23:58 +0100

    deactivated robust dendrogram--updated other dendrograms.

--- pipelineScripts/identifyStates.py
+++ pipelineScripts/identifyStates.py
@@ -36,6 +36,10 @@ trainDat = pd.read_csv(args.dataPath)
 pcaCoords= pd.read_csv(args.PCApath)
 devStates = pd.read_csv(args.devStates, dtype=str, index_col=0)
 
+# Max number of genes in a label of the dendrogram:
+maxInLab = 5
+
+# If there are no deviating states, terminate
 if len(devStates)==0:
     print('no deviating states, terminating...')
     sys.exit()
@@ -44,7 +48,18 @@ if len(devStates)==1:
     print('Only one deviation state, terminating...')
     sys.exit()
 
+
 devStates.columns = ['genes', 'state', 'enrichment', 'pval', 'pval_corrected', 'CellIDs']
+
+# Add the gene states to the devStates dataframe
+def makeGeneState(dtupleDFrow):
+    genes = dtupleDFrow.genes.split('_')
+    state = list(dtupleDFrow.state)
+    zipped =  list(zip(genes, ['+' if s else '-' for s in state]))
+    return [a + b for (a, b) in zipped]
+    
+devStates['geneState'] = devStates.apply(makeGeneState, axis=1)
+
 
 # Binreps is the binary represenations of the interactions: binReps[i] is 1 if cell i is in the deviating state, 0 otherwise.  
 binReps = np.array(devStates.apply(lambda x: (trainDat[x['genes'].rsplit('_')]==[int(g) for g in list(str(x['state']))]).all(axis=1), axis=1))*1
@@ -155,8 +170,6 @@ stateDF = pd.DataFrame(stateDF)
 stateDF.columns = ['State', 'AU', 'BP']
 stateDF.to_csv('statesWithBootstrapStats.csv')
 
-
-d = dendrogram(linked_full, labels = labsWithStates_str, distance_sort='descending', no_plot=True)
 xcoord = d["icoord"]
 ycoord = d["dcoord"]
 # Obtaining the coordinates of all nodes above leaves
@@ -210,129 +223,128 @@ plt.close()
 
 
 
-# Keep all states that have significant bootstrap stats
-AU = pvalues[:, 0]
-states = []
-for i, merge in enumerate(linked_full):
-    if AU[i] >=auThreshold:
-        states.append(clusterDict[n+i])
+# # Keep all states that have significant bootstrap stats
+# AU = pvalues[:, 0]
+# states = []
+# for i, merge in enumerate(linked_full):
+#     if AU[i] >=auThreshold:
+#         states.append(clusterDict[n+i])
         
-#       if one of the two merged clusters is original and get merged with a signficiant cluster, add it separately as well
-        if ((merge[0]<n) & (merge[1]>=n)):
-            if AU[int(merge[1]-(n))]>=auThreshold:
-                states.append([int(merge[0])])
+# #       if one of the two merged clusters is original and get merged with a signficiant cluster, add it separately as well
+#         if ((merge[0]<n) & (merge[1]>=n)):
+#             if AU[int(merge[1]-(n))]>=auThreshold:
+#                 states.append([int(merge[0])])
             
-        elif ((merge[1]<n) & (merge[0]>=n)):
-            if AU[int(merge[0]-(n))]>=auThreshold:
-                states.append([int(merge[1])])
+#         elif ((merge[1]<n) & (merge[0]>=n)):
+#             if AU[int(merge[0]-(n))]>=auThreshold:
+#                 states.append([int(merge[1])])
 
-states = [[int(x) for x in state] for state in states ]
-singletonStates = [[i] for i in range(n) if not i in set.union(*[set(state) for state in states])]
-for s in singletonStates:
-    states.append(s)
+# states = [[int(x) for x in state] for state in states ]
+# singletonStates = [[i] for i in range(n) if not i in set.union(*[set(state) for state in states])]
+# for s in singletonStates:
+#     states.append(s)
 
-# Create binary representation of the significant states:
-fullStateBinReps = []
-for stateList in states:
-    stateList_binRep = [binReps[state] for state in stateList]
-    fullStateBinRep = np.array(stateList_binRep).any(axis=0)*1
-    fullStateBinReps.append(fullStateBinRep)
-fullStateBinReps = np.array(fullStateBinReps)
-
-
-if len(fullStateBinReps)==0:
-    print('No robust combined states, terminating...')
-    sys.exit()
-
-if len(fullStateBinReps)==1:
-    print('Only one robust combined state, terminating...')
-    sys.exit()
+# # Create binary representation of the significant states:
+# fullStateBinReps = []
+# for stateList in states:
+#     stateList_binRep = [binReps[state] for state in stateList]
+#     fullStateBinRep = np.array(stateList_binRep).any(axis=0)*1
+#     fullStateBinReps.append(fullStateBinRep)
+# fullStateBinReps = np.array(fullStateBinReps)
 
 
-# Redo the bootstrap analysis with the significant states:
-X = pd.DataFrame(fullStateBinReps.T)
-pv = PvClust(X, method="average", metric="dice", nboot=bsResamps, parallel=True)
-print('Done with second clustering')
+# if len(fullStateBinReps)==0:
+#     print('No robust combined states, terminating...')
+#     sys.exit()
 
-# Create the labels for the significant states from the top 6 occuring genes
-labs = []
-for state in states:
-    genes = list(map(lambda x: [''.join(g) for g in list(zip(devStates.iloc[x]['genes'].split('_'), ['+' if int(s)==1 else '-' for s in devStates.iloc[x]['state']]))], state))
-    gs = np.array([a for b in genes for a in b])
-    labs.append('\n'.join(np.unique(gs)[np.argsort(-np.unique(gs, return_counts=True)[1])][:6]))
+# if len(fullStateBinReps)==1:
+#     print('Only one robust combined state, terminating...')
+#     sys.exit()
 
-# Create the linkage matrix and dendrogram for the significant states only:
-linked_sig = linkage(fullStateBinReps, 'average', metric='dice')
-d = dendrogram(linked_sig, labels = labs, distance_sort='descending', no_plot=True)
 
-# Create image labels for the significant states:
-sns.set_style('white')
-statePlots = {}
-alph=0.8
-for img, leaf in enumerate(d['leaves']):    
-    plt.figure(figsize=[6, 3])
-    plt.plot(pcaCoords.values[:, 0], pcaCoords.values[:, 1], '.', color=(0.5, 0.5, 0.5, 0.05))
+# # Redo the bootstrap analysis with the significant states:
+# X = pd.DataFrame(fullStateBinReps.T)
+# pv = PvClust(X, method="average", metric="dice", nboot=bsResamps, parallel=True)
+# print('Done with second clustering')
+
+# # Create the labels for the significant states from the top occuring genes
+# labs = []
+# for state in states:
+#     genes = list(map(lambda x: [''.join(g) for g in list(zip(devStates.iloc[x]['genes'].split('_'), ['+' if int(s)==1 else '-' for s in devStates.iloc[x]['state']]))], state))
+#     gs = np.array([a for b in genes for a in b])
+#     labs.append('\n'.join(np.unique(gs)[np.argsort(-np.unique(gs, return_counts=True)[1])][:6]))
+
+# # Create the linkage matrix and dendrogram for the significant states only:
+# linked_sig = linkage(fullStateBinReps, 'average', metric='dice')
+# d = dendrogram(linked_sig, labels = labs, distance_sort='descending', no_plot=True)
+
+# # Create image labels for the significant states:
+# sns.set_style('white')
+# statePlots = {}
+# alph=0.8
+# for img, leaf in enumerate(d['leaves']):    
+#     plt.figure(figsize=[6, 3])
+#     plt.plot(pcaCoords.values[:, 0], pcaCoords.values[:, 1], '.', color=(0.5, 0.5, 0.5, 0.05))
     
-    for stateIndex in states[leaf]:
-        genes = devStates.iloc[stateIndex]['genes'].rsplit('_')
-        state = [int(x) for x in devStates.iloc[stateIndex]['state']]
-        charCells = (trainDat[genes]==state).all(axis=1)
-        plt.plot(pcaCoords.values[charCells.astype(bool), 0], pcaCoords.values[charCells.astype(bool), 1], 'o', alpha=alph)
+#     for stateIndex in states[leaf]:
+#         genes = devStates.iloc[stateIndex]['genes'].rsplit('_')
+#         state = [int(x) for x in devStates.iloc[stateIndex]['state']]
+#         charCells = (trainDat[genes]==state).all(axis=1)
+#         plt.plot(pcaCoords.values[charCells.astype(bool), 0], pcaCoords.values[charCells.astype(bool), 1], 'o', alpha=alph)
         
-    plt.xticks([])
-    plt.yticks([])
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png', bbox_inches='tight',pad_inches = 0)
-    buf.seek(0)
-    statePlots[img] = fromImToArr(Image.open(buf))
-    plt.close()
+#     plt.xticks([])
+#     plt.yticks([])
+#     buf = io.BytesIO()
+#     plt.savefig(buf, format='png', bbox_inches='tight',pad_inches = 0)
+#     buf.seek(0)
+#     statePlots[img] = fromImToArr(Image.open(buf))
+#     plt.close()
 
-xcoord = d["icoord"]
-ycoord = d["dcoord"]
-# Obtaining the coordinates of all nodes above leaves
-x = {i: (j[1]+j[2])/2 for i, j in enumerate(xcoord)}
-y = {i: j[1] for i, j in enumerate(ycoord)}
-pos = node_positions(y, x)
+# xcoord = d["icoord"]
+# ycoord = d["dcoord"]
+# # Obtaining the coordinates of all nodes above leaves
+# x = {i: (j[1]+j[2])/2 for i, j in enumerate(xcoord)}
+# y = {i: j[1] for i, j in enumerate(ycoord)}
+# pos = node_positions(y, x)
 
-plt.figure(figsize=[len(fullStateBinReps)+20, 20])     
-d = dendrogram(linked_sig, labels=labs, above_threshold_color='c',
-               color_threshold=0.0, leaf_font_size=15)
+# plt.figure(figsize=[len(fullStateBinReps)+20, 20])     
+# d = dendrogram(linked_sig, labels=labs, above_threshold_color='c',
+#                color_threshold=0.0, leaf_font_size=15)
 
-pvalues = pv._result[['AU', 'BP']].values
-ax = plt.gca()
-for node, (x, y) in pos.items():
+# pvalues = pv._result[['AU', 'BP']].values
+# ax = plt.gca()
+# for node, (x, y) in pos.items():
 
-    if node == (len(pos.items())-1):
-        ax.text(x-6, y, 'AU', fontsize=14, fontweight='bold',
-                color='black')
-        ax.text(x+1, y, 'BP', fontsize=14, fontweight='bold',
-                color='black')
-    else:
-        if pvalues[node][0] >= auThreshold:
-            ax.text(x-5, y, f' {pvalues[node][0]*100:.0f}', fontsize=10,
-                    color='green', fontweight='bold')
-            ax.text(x+1, y, f'{pvalues[node][1]*100:.0f}', fontsize=10,
-                    color='green', fontweight='bold')
-        else:
-            ax.text(x-5, y, f' {pvalues[node][0]*100:.0f}', fontsize=10,
-                    color='black')
-            ax.text(x+1, y, f'{pvalues[node][1]*100:.0f}', fontsize=10,
-                    color='black')
+#     if node == (len(pos.items())-1):
+#         ax.text(x-6, y, 'AU', fontsize=14, fontweight='bold',
+#                 color='black')
+#         ax.text(x+1, y, 'BP', fontsize=14, fontweight='bold',
+#                 color='black')
+#     else:
+#         if pvalues[node][0] >= auThreshold:
+#             ax.text(x-5, y, f' {pvalues[node][0]*100:.0f}', fontsize=10,
+#                     color='green', fontweight='bold')
+#             ax.text(x+1, y, f'{pvalues[node][1]*100:.0f}', fontsize=10,
+#                     color='green', fontweight='bold')
+#         else:
+#             ax.text(x-5, y, f' {pvalues[node][0]*100:.0f}', fontsize=10,
+#                     color='black')
+#             ax.text(x+1, y, f'{pvalues[node][1]*100:.0f}', fontsize=10,
+#                     color='black')
 
-ax = plt.gca()
-x_labLocs = [x.get_position()[0] for x in ax.get_xmajorticklabels()]
+# ax = plt.gca()
+# x_labLocs = [x.get_position()[0] for x in ax.get_xmajorticklabels()]
 
-# Adding the PCA embeddings as labels to the dendrogram plot
-for i, xL in enumerate(x_labLocs):
-    add_imgLab(xL, -0.03, statePlots[i], ax, zoom=0.18)
+# # Adding the PCA embeddings as labels to the dendrogram plot
+# for i, xL in enumerate(x_labLocs):
+#     add_imgLab(xL, -0.03, statePlots[i], ax, zoom=0.18)
     
-plt.xticks(x_labLocs, labels = d['ivl'], rotation=0, fontsize=10)    
-sns.despine(left=True, top=True, right=True, bottom=True)
-plt.ylabel('Dice-distance')
-plt.ylim(-0.03, 1)
-plt.savefig('dendrogram_reclustered_robust_states.png', bbox_inches='tight')
-plt.close()
-
+# plt.xticks(x_labLocs, labels = d['ivl'], rotation=0, fontsize=10)    
+# sns.despine(left=True, top=True, right=True, bottom=True)
+# plt.ylabel('Dice-distance')
+# plt.ylim(-0.03, 1)
+# plt.savefig('dendrogram_reclustered_robust_states.png', bbox_inches='tight')
+# plt.close()
 
 
 
@@ -347,15 +359,16 @@ devStates.to_csv(f'top_DTuples_withStatesFromCut.csv')
 statePlots = {}
 truncatedClusters = max(devStates['cluster']) # The total number of clusters after truncation
 
-# Labels that combine the genes and their states---once as list, once as string with underscores
 
 R_full = dendrogram(linked_full, labels = labsWithStates_str, distance_sort='descending', no_plot=True)
 R_trunc =dendrogram(linked_full, labels = labsWithStates_str, distance_sort='descending', no_plot=True, p=truncatedClusters, truncate_mode='lastp')
 
 intsInClusters = []
+cellsInClusters = []
 index = 0
 for img, geneStr in enumerate(R_trunc['ivl']):
     cl = []
+    cellNumber = 0
     plt.figure(figsize=[6, 3])
 
     # Truncation renames a cluster to '(n)' if it is the result of merging n clusters
@@ -372,6 +385,7 @@ for img, geneStr in enumerate(R_trunc['ivl']):
         charCells = (trainDat[genes]==state).all(axis=1)
         plt.plot(pcaCoords.values[charCells, 0], pcaCoords.values[charCells, 1], 'o', alpha=alph)
         cl.append([''.join(s) for s in list(zip(genes, ['+' if x==1 else '-' for x in state]))])
+        cellNumber += charCells.sum()
         index+=1
 
     else:
@@ -390,10 +404,12 @@ for img, geneStr in enumerate(R_trunc['ivl']):
             charCells = (trainDat[genes]==state).all(axis=1)
             plt.plot(pcaCoords.values[charCells, 0], pcaCoords.values[charCells, 1], 'o', alpha=alph)
             cl.append([''.join(s) for s in list(zip(genes, ['+' if x==1 else '-' for x in state]))])
+            cellNumber += charCells.sum()
         index += toAdd
     
     
     intsInClusters.append(cl)
+    cellsInClusters.append(cellNumber)
     plt.xticks([])
     plt.yticks([])
     buf = io.BytesIO()
@@ -421,12 +437,16 @@ def add_imgLab(xCoord, yCoord, img, ax):
 
 sns.set_style('white')
 
-# create list of top 4 occuring genes across interactions in a cluster to add to the label
-labs = []
-for c in intsInClusters:
-    gs = np.array([a for b in c for a in b])
-    labs.append('\n'.join(np.unique(gs)[np.argsort(-np.unique(gs, return_counts=True)[1])][:4]))
+# create list of top occuring genes across interactions in a cluster to add to the label
 
+labs = []
+for (cellNr, intList) in list(zip(cellsInClusters, intsInClusters)):
+    gs = np.array([a for b in intList for a in b])
+    sortedGenes = list(np.unique(gs)[np.argsort(-np.unique(gs, return_counts=True)[1])])
+    if len(sortedGenes) > maxInLab:
+        sortedGenes = sortedGenes[:maxInLab] + [f'...']
+    sortedGenes = sortedGenes + [f'({len(intList)} d-tuples)'] + [f'({cellNr} cells)']
+    labs.append('\n'.join(sortedGenes))
 
 plt.figure(figsize=(truncatedClusters*2, 5))
 R = dendrogram(linked_full,
