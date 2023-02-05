cfbb40b430c43f3da7c479fa8f30dcd89185fd3c
Abel Jansma <abel_jansma@hotmail.com>
2023-02-05 18:28:50 +0100

    debugging

--- pipelineScripts/identifyStates.py
+++ pipelineScripts/identifyStates.py
@@ -74,6 +74,7 @@ labsWithStates_str = labsWithStates.apply(lambda x: '\n'.join(x)).values
 cellCounts = binReps.sum(axis=0)
 labsWithStates_str = list(map(lambda x: x[0]+f'\n({x[1]} cells)', list(zip(labsWithStates_str, cellCounts))))
 print(labsWithStates_str)
+print(binReps)
 
 # linkage defines the distances between the binReps, using the Dice-distance: https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient
 linked_full = linkage(binReps, 'average', metric='dice')
