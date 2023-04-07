eee121248a2dffbb196b444e640f799a30c282ee
Abel Jansma <abel_jansma@hotmail.com>
2023-04-07 11:44:28 +0200

    debugging

--- pipelineScripts/createHOIsummaries.py
+++ pipelineScripts/createHOIsummaries.py
@@ -39,10 +39,9 @@ import os
 def printRAM():
     # Getting all memory using os.popen()
     total_memory, used_memory, free_memory = map(
-        int, os.popen('free -t -m').readlines()[-1].split()[1:])
+        int, os.popen('free').readlines()[-1].split()[1:])
     # Memory usage
-    print("RAM memory % used:", round((used_memory/total_memory) * 100, 2))
-
+    print("RAM memory % used:", round((float(used_memory)/total_memory) * 100, 2))
 
 
 
