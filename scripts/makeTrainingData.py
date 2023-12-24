55c03f5743759db22cf84e739761a6bdf04801cd
Abel Jansma <abel@Radix.local>
2023-12-24 16:38:56 +0100

    bug fix

--- scripts/makeTrainingData.py
+++ scripts/makeTrainingData.py
@@ -2,7 +2,6 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
-import scrublet as scr
 import scanpy as sc
 sc.settings.figdir = ''
 import pandas as pd
